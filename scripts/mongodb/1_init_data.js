dc3600cbdaf0a0c24f27cad548f5f0d49e61a425
SeanChao <seanchao0804@gmail.com>
2020-11-18 20:53:59 +0800

    fix: mongo test script

--- scripts/mongodb/1_init_data.js
+++ scripts/mongodb/1_init_data.js
@@ -1,3 +1,3 @@
-db.replies.insert({rid: 1, content: '总之就是非常可爱'})
+db.replies.insert({rid: 1, text: '总之就是非常可爱'})
 db.questions.insert({qid: 1, detail: 'BRAVO~'})
 db.answers.insert({aid: 1, text: '哦我的老伙计，这可真是个好问题！'})
