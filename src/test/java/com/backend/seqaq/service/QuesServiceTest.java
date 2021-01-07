8e92469ad5326bf4500498fed3fda3c7fc8a50ab
SeanChao <seanchao0804@gmail.com>
2021-01-07 23:54:05 +0800

    fix: test profile

--- src/test/java/com/backend/seqaq/service/QuesServiceTest.java
+++ src/test/java/com/backend/seqaq/service/QuesServiceTest.java
@@ -5,11 +5,13 @@ import com.backend.seqaq.entity.Questions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class QuesServiceTest {
   @Autowired private QuesService quesService;
